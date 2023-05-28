25e0fde5264a535a31bb5c9414691b110fa3ef8d
shenkha <21522189@ms.uit.edu.vn>
2023-05-28 16:04:15 +0700

    Pacman Project

---
+++ Assignment3/multiagent/script.sh
@@ -0,0 +1,72 @@
+python pacman.py -n 5 -l capsuleClassic -p MinimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l capsuleClassic -p AlphaBetaAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l capsuleClassic -p ExpectimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l contestClassic -p MinimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l contestClassic -p AlphaBetaAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l contestClassic -p ExpectimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l mediumClassic -p MinimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l mediumClassic -p AlphaBetaAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l mediumClassic -p ExpectimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l minimaxClassic -p MinimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l minimaxClassic -p AlphaBetaAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l minimaxClassic -p ExpectimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l openClassic -p MinimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l openClassic -p AlphaBetaAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l openClassic -p ExpectimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l originalClassic -p MinimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l originalClassic -p AlphaBetaAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l originalClassic -p ExpectimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l capsuleClassic -p MinimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l capsuleClassic -p AlphaBetaAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l capsuleClassic -p ExpectimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l contestClassic -p MinimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l contestClassic -p AlphaBetaAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l contestClassic -p ExpectimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l mediumClassic -p MinimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l mediumClassic -p AlphaBetaAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l mediumClassic -p ExpectimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l minimaxClassic -p MinimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l minimaxClassic -p AlphaBetaAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l minimaxClassic -p ExpectimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l openClassic -p MinimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l openClassic -p AlphaBetaAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l openClassic -p ExpectimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l originalClassic -p MinimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l originalClassic -p AlphaBetaAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l originalClassic -p ExpectimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q
+python pacman.py -n 5 -l capsuleClassic -p MinimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l capsuleClassic -p AlphaBetaAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l capsuleClassic -p ExpectimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l contestClassic -p MinimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l contestClassic -p AlphaBetaAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l contestClassic -p ExpectimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l mediumClassic -p MinimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l mediumClassic -p AlphaBetaAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l mediumClassic -p ExpectimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l minimaxClassic -p MinimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l minimaxClassic -p AlphaBetaAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l minimaxClassic -p ExpectimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l openClassic -p MinimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l openClassic -p AlphaBetaAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l openClassic -p ExpectimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l originalClassic -p MinimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l originalClassic -p AlphaBetaAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l originalClassic -p ExpectimaxAgent -a depth=2,evalFn=betterEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l capsuleClassic -p MinimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l capsuleClassic -p AlphaBetaAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l capsuleClassic -p ExpectimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l contestClassic -p MinimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l contestClassic -p AlphaBetaAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l contestClassic -p ExpectimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l mediumClassic -p MinimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l mediumClassic -p AlphaBetaAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l mediumClassic -p ExpectimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l minimaxClassic -p MinimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l minimaxClassic -p AlphaBetaAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l minimaxClassic -p ExpectimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l openClassic -p MinimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l openClassic -p AlphaBetaAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l openClassic -p ExpectimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l originalClassic -p MinimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l originalClassic -p AlphaBetaAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
+python pacman.py -n 5 -l originalClassic -p ExpectimaxAgent -a depth=2,evalFn=scoreEvaluationFunction -f --frameTime 0 -q -g DirectionalGhost
